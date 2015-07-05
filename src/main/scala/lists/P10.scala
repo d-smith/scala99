783ef1fbf82d4e059088ced7ccd580dac73b7c25
d-smith <doug.smith.mail@gmail.com>
2015-07-05 08:53:34 -0600

    Added modified run-length encoding

--- src/main/scala/lists/P10.scala
+++ src/main/scala/lists/P10.scala
@@ -6,3 +6,11 @@ object P10 {
     pack(l) map { e => (e.length, e.head)}
   }
 }
+
+object P11 {
+  import P10.encode
+
+  def encodeModified[A](l : List[A]) : List[Any] = {
+    encode(l) map { e => if (e._1 == 1) e._2 else e }
+  }
+}
--- src/test/scala/lists/P10Test.scala
+++ src/test/scala/lists/P10Test.scala
@@ -10,3 +10,12 @@ class P10Test extends FunSuite {
     assert(encode(l) === List((4,'a),(1,'b),(2,'c), (2,'a), (1,'d), (4,'e)))
   }
 }
+
+class P11Test extends FunSuite {
+  import P11.encodeModified
+
+  test("single elements in run-length encoded lists are copied directly") {
+    val l = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    assert(encodeModified(l) === List((4,'a),'b,(2,'c), (2,'a), 'd, (4,'e)))
+  }
+}
