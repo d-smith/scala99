91f9e886279fa1d2665bc4c77cf492b2bd305914
d-smith <doug.smith.mail@gmail.com>
2015-08-13 07:21:33 -0600

    Added or test case, refactored tests to generalize how to test the logical fns

--- src/test/scala/logic/P99LogicTest.scala
+++ src/test/scala/logic/P99LogicTest.scala
@@ -4,19 +4,39 @@ import org.scalatest.FunSuite
 
 
 class P99LogicTest extends FunSuite {
-
   import P99Logic._
-
-  val input = List(
+  
+  val andTable = List(
     (false, false, false),
     (false, true, false),
     (true, false, false),
     (true, true, true)
   )
 
-  input.foreach{i =>
-    test(s"Test of and(${i._1}, ${i._2})") {
-      assert(and(i._1, i._2) === i._3)
+  val orTable = List(
+    (false, false, false),
+    (false, true, true),
+    (true, false, true),
+    (true, true, true)
+  )
+
+
+  val andFn: (Boolean, Boolean) => Boolean = and
+  val orFn: (Boolean, Boolean) => Boolean = or
+
+  val testData = List(
+    ("and", andFn, andTable),
+    ("or", orFn, orTable)
+  )
+
+  testData.foreach{ td =>
+    val (testName, testFn, testTable) = td
+    testTable.foreach{ tt =>
+      val testcase = s"Test of $testName(${tt._1}, ${tt._2})"
+      println(testcase)
+      test(testcase) {
+        assert(testFn(tt._1,tt._2) === tt._3)
+      }
     }
   }
 }
