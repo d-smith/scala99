a734a1ded7ca23aa4f84ba17b9a346fd57e696db
d-smith <doug.smith.mail@gmail.com>
2015-08-14 07:03:19 -0600

    Finished logical tests

--- src/test/scala/logic/P99LogicTest.scala
+++ src/test/scala/logic/P99LogicTest.scala
@@ -4,8 +4,9 @@ import org.scalatest.FunSuite
 
 
 class P99LogicTest extends FunSuite {
+
   import P99Logic._
-  
+
   val andTable = List(
     (false, false, false),
     (false, true, false),
@@ -20,22 +21,66 @@ class P99LogicTest extends FunSuite {
     (true, true, true)
   )
 
+  val nandTable = List(
+    (false, false, true),
+    (false, true, true),
+    (true, false, true),
+    (true, true, false)
+  )
+
+  val norTable = List(
+    (false, false, true),
+    (false, true, false),
+    (true, false, false),
+    (true, true, false)
+  )
+
+  val equTable = List(
+    (false, false, true),
+    (false, true, false),
+    (true, false, false),
+    (true, true, true)
+  )
+
+  val xorTable = List(
+    (false, false, false),
+    (false, true, true),
+    (true, false, true),
+    (true, true, false)
+  )
+
+  val implTable = List(
+    (false, false, true),
+    (false, true, true),
+    (true, false, false),
+    (true, true, true)
+  )
+
 
   val andFn: (Boolean, Boolean) => Boolean = and
   val orFn: (Boolean, Boolean) => Boolean = or
+  val nandFn: (Boolean, Boolean) => Boolean = nand
+  val norFn:(Boolean,Boolean) => Boolean = nor
+  val equFn:(Boolean, Boolean) => Boolean = equ
+  val xorFn:(Boolean,Boolean) => Boolean = xor
+  val implFn:(Boolean,Boolean) => Boolean = impl
 
   val testData = List(
     ("and", andFn, andTable),
-    ("or", orFn, orTable)
+    ("or", orFn, orTable),
+    ("nand", nandFn, nandTable),
+    ("nor", norFn, norTable),
+    ("equ",equFn, equTable),
+    ("xor",xorFn, xorTable),
+    ("impl", implFn, implTable)
   )
 
-  testData.foreach{ td =>
+  testData.foreach { td =>
     val (testName, testFn, testTable) = td
-    testTable.foreach{ tt =>
+    testTable.foreach { tt =>
       val testcase = s"Test of $testName(${tt._1}, ${tt._2})"
-      println(testcase)
       test(testcase) {
-        assert(testFn(tt._1,tt._2) === tt._3)
+        assert(testFn(tt._1, tt._2) === tt._3)
       }
     }
   }
